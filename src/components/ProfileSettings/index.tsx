1d129018ae8ecb9ec49ee1dca5927db6eb95bac3
Snahier <hansalmeida@hotmail.com>
2020-08-29 07:58:01 -0300

    Add Profile settings component

---
+++ src/components/ProfileSettings/index.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import { ProfileSettingsContainer } from "./styles"
+import { ThreeDotsVertical } from "@styled-icons/bootstrap"
+
+interface ProfileSettingsProps {}
+
+const ProfileSettings: React.FC<ProfileSettingsProps> = () => {
+  return (
+    <ProfileSettingsContainer>
+      <img
+        src="https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+        alt=""
+      />
+      <ThreeDotsVertical width={16} color="#fff" />
+    </ProfileSettingsContainer>
+  )
+}
+export default ProfileSettings
---
+++ src/components/ProfileSettings/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+export const ProfileSettingsContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  cursor: pointer;
+
+  img {
+    width: 30px;
+    height: 30px;
+    object-fit: cover;
+    border-radius: 50%;
+
+    box-sizing: border-box;
+    border: 4px solid #3092dc;
+  }
+`
