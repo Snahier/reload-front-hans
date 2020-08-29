c891304134f5a7706c6d7aba7c0f2b9f272adf1d
Snahier <hansalmeida@hotmail.com>
2020-08-29 16:44:38 -0300

    Increase outline cards size

--- src/pages/Profile/Graphics/AirQuality/styles.ts
+++ src/pages/Profile/Graphics/AirQuality/styles.ts
@@ -29,7 +29,7 @@ export const AirQualityContainer = styled.section`
     font-weight: 300;
     font-size: 0.9rem;
 
-    padding: 0.5rem 1rem;
+    padding: 0.8rem 2rem;
 
     transition: background-color 0.2s;
   }
